99572c90f0683495146b1b5d3c26c45eb2971c54
Jiří Poledník <106665722+Jiri-Polednik@users.noreply.github.com>
2024-05-29 15:18:41 +0200

    fixes

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -26,102 +26,84 @@ export default function Index({
 
   return (
     <div>
-      <div className={"flex min-h-dvh w-full"}>
-        <div className={"flex h-full w-full justify-center"}>
-          <Image
-            src={"/images/heroBannerBackground.jpg"}
-            alt={"Logo"}
-            fill
-            className={"object-cover"}
-          />
-          <div
-            className={"absolute top-0 w-full h-full bg-black bg-opacity-60"}
-          />
-          <div className={"absolute top-0 w-full h-full flex justify-center"}>
-            <div
-              className={"flex flex-col justify-center items-center px-8 gap-8"}
-            >
-              <p className={"text-4xl font-bold text-white text-center"}>
-                {t("HomePage.welcome")}
-              </p>
-              <p className={"text-2xl text-white text-center"}>
-                {t("HomePage.subtitle")}
-              </p>
-            </div>
+      <div className="hero flex w-full">
+        <Image
+          src="/images/heroBannerBackground.jpg"
+          alt="Logo"
+          fill
+          className="object-cover w-full h-full"
+        />
+        <div className="absolute top-0 w-full h-full bg-black bg-opacity-60" />
+        <div className="absolute top-0 w-full h-full flex justify-center">
+          <div className="flex flex-col justify-center items-center px-8 gap-8">
+            <p className="text-4xl font-bold text-white text-center">
+              {t("HomePage.welcome")}
+            </p>
+            <p className="text-2xl text-white text-center">
+              {t("HomePage.subtitle")}
+            </p>
           </div>
         </div>
       </div>
 
-      <div className={"flex justify-center bg-blue-light"}>
-        <p className={"text-4xl font-bold text-center text-gray-dark mt-4"}>
+      <div className="flex justify-center bg-blue-light">
+        <p className="text-4xl font-bold text-center text-gray-dark mt-4">
           {t("HomePage.title")}
         </p>
       </div>
 
       <PageSection
         leftContent={
-          <div className={"flex justify-center"}>
-            <div className={"flex justify-center w-2/3"}>
+          <div className="flex justify-center">
+            <div className="flex justify-center w-2/3">
               <Image
-                src={"/images/aboutMe.jpg"}
-                alt={"Image 1"}
+                src="/images/aboutMe.jpg"
+                alt="Image 1"
                 width={500}
                 height={500}
-                className={"rounded-2xl my-auto"}
+                className="rounded-2xl my-auto"
               />
             </div>
           </div>
         }
         rightContent={
-          <div
-            className={
-              "w-2/3 flex-col justify-center items-center text-gray-dark"
-            }
-          >
-            <p className={"text-center mt-4"}>
-              {t("HomePage.description.one")}
-            </p>
-            <p className={"text-center mt-4"}>
-              {t("HomePage.description.two")}
-            </p>
-            <p className={"text-center mt-4"}>
+          <div className="w-2/3 flex-col justify-center items-center text-gray-dark">
+            <p className="text-center mt-4">{t("HomePage.description.one")}</p>
+            <p className="text-center mt-4">{t("HomePage.description.two")}</p>
+            <p className="text-center mt-4">
               {t("HomePage.description.three")}
             </p>
           </div>
         }
-        bgColor={"blue-light"}
+        bgColor="blue-light"
       />
       <PageSection
-        bgColor={"blue-dark"}
+        bgColor="blue-dark"
         content={
-          <div
-            className={
-              "flex flex-col justify-center text-gray-light w-full gap-8 py-5"
-            }
-          >
+          <div className="flex flex-col justify-center text-gray-light w-full gap-8 py-5">
             <div>
-              <p className={"text-2xl font-bold text-center"}>
+              <p className="text-2xl font-bold text-center">
                 {t("HomePage.explore")}
               </p>
             </div>
-            <div className={"flex gap-4 justify-center"}>
+            <div className="flex gap-4 justify-center">
               <Link href="/web2" className="cursor-pointer">
-                <Button variant={"full"} bgColor={"blue-dark"}>
+                <Button variant="full" bgColor="blue-dark">
                   {t("HomePage.web2")}
                 </Button>
               </Link>
               <Link href="/web3" className="cursor-pointer">
-                <Button variant={"full"} bgColor={"blue-dark"}>
+                <Button variant="full" bgColor="blue-dark">
                   {t("HomePage.web3")}
                 </Button>
               </Link>
             </div>
 
             <div>
-              <p className={"text-center mt-4"}>{t("HomePage.email")}</p>
+              <p className="text-center mt-4">{t("HomePage.email")}</p>
             </div>
 
-            <div className={"flex gap-4 justify-center"}>
+            <div className="flex gap-4 justify-center">
               <a
                 href={t("HomePage.linkedinLink")}
                 target="_blank"
@@ -129,8 +111,8 @@ export default function Index({
                 className="mx-1"
               >
                 <Image
-                  src={"/images/linkedin.svg"}
-                  alt={"LinkedIn"}
+                  src="/images/linkedin.svg"
+                  alt="LinkedIn"
                   width={50}
                   height={50}
                 />
@@ -142,8 +124,8 @@ export default function Index({
                 className="mx-1"
               >
                 <Image
-                  src={"/images/github.svg"}
-                  alt={"GitHub"}
+                  src="/images/github.svg"
+                  alt="GitHub"
                   width={50}
                   height={50}
                 />
